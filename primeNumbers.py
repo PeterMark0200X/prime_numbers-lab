e7996e0978524b2fbeb5dfb7a65d8e1580a6908b
PeterMark0200X <39591138+PeterMark0200X@users.noreply.github.com>
2018-11-17 14:08:28 +0300

    Add files via upload

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,1 +1,21 @@
+import math
+
+def genPrimes(n):
+	for i in range(2, int(math.sqrt(n)+1)):
+		if n % i == 0: 
+			return False;
+	return n>1;
+	
+num = int(input('enter the maximum range eg"100/300"; '))
+prime = []
+
+def primeNumbers(num):
+	if num <= 0:
+		return prime
+	else:
+		for n in range (num):
+			if genPrimes(n):
+				prime.append(n)
+		return prime
+		
 
